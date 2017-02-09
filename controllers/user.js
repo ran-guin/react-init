5a4a5babf4bdfec891ab097fa7994c3071402dec
Ran Guin <ran.guin@gmail.com>
2017-02-09 17:27:42 +0700

    add db connection for user

--- controllers/user.js
+++ controllers/user.js
@@ -3,10 +3,38 @@ var router = express.Router();
 var Dbc = require('../models/dbc');
 var jwt = require('jsonwebtoken');
 
-router.get('/',function(req,res) {
-  var dbc = new Dbc();
-  dbc.loadUser();
-  res.json({error: false, message: 'temp.. remove'});
+router.get('/:userid',function(req,res) {
+ 	
+ 	var userid = req.params.userid;
+
+ 	if (userid && userid.match(/^\d+$/)) {
+		var dbc = new Dbc();
+
+ 		dbc.connect()
+ 		.then ( function (connection) {
+ 			console.log("Connected in user module");
+ 			var query = "Select * from user where id = " + userid;
+ 			connection.query(query)
+	 		.then ( function(result) {
+	 			console.log("User test ...: " + JSON.stringify(result));
+	 			connection.end();
+		 		res.json(result);
+	 		})
+	 		.catch( function (err) {
+	 			console.log("Error querying: " + err);
+	 			connection.end();
+	 			res.json(err);
+	 		})
+	 	})
+	 	.catch ( function (err) {
+	 		console.log("Error connecting...");
+
+	 		res.json(err);
+	 	});
+	 }
+	 else {
+	 	res.json({ error: 'No userid supplied'} );
+	 }
 });
 
 router.post('/',function(req,res) {
