0f389bbdb7896c82fa7064232499a86489f0ce9d
Ran Guin <ran.guin@gmail.com>
2017-05-27 20:23:31 -0700

    enable dynamic search

--- client/src/Standard/SearchGrid/SearchGrid.js
+++ client/src/Standard/SearchGrid/SearchGrid.js
@@ -1,118 +1,134 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import axios from 'axios';
+import cors from 'cors';
+
+
+import {observable} from 'mobx';
+import {observer} from 'mobx-react';
+
+import SearchResults from './SearchResults';
 import './SearchGrid.css';
+
+const corsOptions = {
+  origin: true,
+  methods: ['GET','POST'],
+  credentials: true,
+  maxAge: 3600
+};
+
+var searchSettings   = observable({ string: '', caseSensitive: true });
+
+var selected = observable({ count: 0, ids: [], labels: [] });
+
 
 class App extends Component {
 
 	static PropTypes = {
-    	title: React.PropTypes.string,
+		table: React.PropTypes.string,
+		header: React.PropTypes.string,
+		caseSensitive: React.PropTypes.boolean,
     	fields: React.PropTypes.array,
+    	show: React.PropTypes.array,
     	test: React.PropTypes.array,
+    	onPick: React.PropTypes.function,
     }
 
 	constructor(props) {
 		super(props);
-
+		this.state = { 
+			searchResults: null,
+			highlight: null,
+			count: 0,
+			clicks: 0,
+		}
 	}
 
-	componentWillMount(props) {      
-		this.setState({
-			results : []
-		});
+	componentWillMount(props) { 
+		searchSettings.string =  observable('');
+		searchSettings.caseSensitive = observable(this.props.caseSensitive);
 	} 
 
-	searchIt = element => {
-		console.log("Searching");
+	loadResults(load) {
+		this.setState(load);
 	}
 
-	searchForIt() {
-		console.log("Search for data...");
-		this.setState({results: [['a','b'],['c','d']] });
-		console.log("Got results: " + this.state.results);
+	clickit(evt) {
+		this.setState({ count: this.state.count + 1})
+	}
+
+	searchForIt(e) {
+		console.log("Search for data containing..." + searchSettings.string);
+		var searchString = e.target.value;
+
+		var _this = this;
+		var fields = _this.props.fields;
+		var show = _this.props.show || fields;
+
+		var url = 'http://localhost:3002/lookup/search';
+
+		var data = {}; // cors(corsOptions);
+		data.model = this.props.table;
+
+		var conditions = [];
+		
+		for (var i=0; i<fields.length; i++) {
+			conditions.push(fields[i] + " LIKE '%" + searchString + "%'");
+		}
+		data.condition = conditions.join(' OR ');
+
+		console.log('** Search : ' + JSON.stringify(data));
+
+		axios.post(url, data )
+		.then ( function (result) {
+			console.log('got ' + data.model + ' results');
+			var newdata = result.data[0];
+
+			console.log("set results: " + JSON.stringify(newdata)); 
+			// _this.setState({ searchResults: newdata});
+
+			_this.loadResults({searchResults: newdata, fields: show, highlight: searchString});
+
+			console.log(JSON.stringify(result.data[0]));
+		})
+		.catch ( function (err) {
+			_this.data = null;
+			console.log('axios call error');
+		});
 	}
 
 	render() {
 
 		var _this = this;
 		var fields = this.props.fields;
-		var results = this.state.results;
+		var show  = this.props.show || fields;
+		var onPick = this.props.onPick.bind(this);
+
+		var highlight = this.state.highlight;
+		var results = this.state.searchResults;
 
 		var header;
 		var form;
 
-		if (fields && fields.length) {
-	 		header = <tr>
-	            {fields.map(function(field, index) { 
-				
-					console.log("*** " + JSON.stringify(field));
-		              
-		            var heading = field.label || field.name;
-		            
-		            return (<th key={index}> ** {heading} ** </th>)
-	            })}
-	            </tr>
+	    console.log("rendering search results: " + JSON.stringify(results));
 
-	        form = <tr>
-	        	{fields.map(function(field, index) {
-	        		return (
-	        			<td key={index}>
-	        				<input type='text' width='100%'/>
-	        			</td>
-	        		)
-	        	})}
-	        	</tr>
-		}
-	    else {
-	    	return <div>No Fields Specified</div>
-	    }
-
-	    var found;
-		if (results && results.length) {
-			console.log("GOT RESULTS");
-			found = <tbody>
-				{header}
-				{form}
-				{results.map(function(record, i) {
-					return (
-						<tr key={i}>
-		        		{record.map(function(data, j) {
-		        			console.log(i + " Data: " + data);
-			        		var key = i + '.' + j
-			        		return (
-			        			<td key={key}>
-			        				d = {data}
-			        			</td>
-			        		)
-			        	})}
-			        	</tr>
-			        )
-	        	})}
-	        	</tbody>
-
-	        console.log('founded: ' + JSON.toString(found));
-
-	    }
-	    else { 
-	    	found = <tbody>
-	    		{header}
-	    		{form}
-	    		<tr><td>nothing found</td></tr>
-	    		</tbody>
-
-	    	console.log("NO results");
-
-	    }
-
+	    var found = <SearchResults searchResults={results} fields={show} highlight={highlight} onPick={this.props.onPick} />
+	    
         return (  
 			<div className='container SearchGrid'>
 				<h3>Search</h3>
-				<input onClick={this.searchForIt.bind(this)} />
-				<table width='100%'>
+				<input onBlur={this.searchForIt.bind(this)} />
 					{found}
-				</table>
 				<hr />
 			</div>
 		)
 	}
+}
+
+App.defaultProps = {
+	fields: ['id'],
+	test: [],
 }
 
 export default App;
